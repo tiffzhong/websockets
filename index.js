71c06dbb39ce55d225504a2b3da8cd2ee152e3f7
Tiffany Zhong <tiffzhong@gmail.com>
2019-03-30 19:16:42 -0700

    setting up socket server

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const socket = require("socket.io");
 const app = express();
 
 const PORT = 4000;
-app.listen(PORT, function() {
+const server = app.listen(PORT, function() {
   console.log(`listening on port ${PORT}`);
 });
 
 app.use(express.static("public"));
+
+//socket set up
+const io = socket(server);
+io.on("connection", function(socket) {
+  console.log("socket connected");
+});
---
+++ public/chat.js
@@ -0,0 +1,2 @@
+//connection
+let socket = io.connect("http://localhost:4000");
